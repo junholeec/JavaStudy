d3f20592a96f34829a99ef21fa64c1b5e31c9558
junholeec <idzuno@gmail.com>
2023-06-11 22:09:58 +0900

    [Modern Java in action] 테스트 데이터 추가
    - 스트림 학습용 테스트 데이터 추가

---
+++ src/main/java/modern/java/in/action/data/Dish.java
@@ -0,0 +1,38 @@
+package modern.java.in.action.data;
+
+public class Dish {
+    private final String name;
+    private final boolean vegetarian;
+    private final int calories;
+    private final Type type;
+
+    public Dish(String name, boolean vegetarian, int calories, Type type) {
+        this.name = name;
+        this.vegetarian = vegetarian;
+        this.calories = calories;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s=%d", name, calories);
+    }
+
+    public enum Type { MEAT, FISH, OTHER }
+}
